0e4a0ff81e96d97de29919f6cd6802d5b473a51d
Noah Cuz <sales@noahcuz.com>
2023-12-27 23:45:02 -0500

    dock initial animation

--- components/layout/Dock.tsx
+++ components/layout/Dock.tsx
@@ -8,12 +8,13 @@ import {
   MotionValue,
   motion,
   useMotionValue,
+  AnimatePresence,
   useSpring,
   useTransform
 } from 'framer-motion';
 import { useRef } from 'react';
 
-import { GalleryHorizontalEnd, Send, FileDown, User, Home } from 'lucide-react';
+import { GalleryVerticalEnd, Send, FileDown, User, Home } from 'lucide-react';
 
 import {
   Tooltip,
@@ -35,7 +36,7 @@ const mainRoutes = [
   },
   {
     label: 'Portfolio',
-    icon: GalleryHorizontalEnd,
+    icon: GalleryVerticalEnd,
     href: '/portfolio'
   },
   {
@@ -51,38 +52,37 @@ const mainRoutes = [
 ];
 
 export default function Dock() {
-  let mouseX = useMotionValue(Infinity);
+  const initialY = 125; // Initial Y position for swipe-up animation
+  const animationDelay = 1; // Delay in seconds for the animation to start
+
+  const mouseX = useMotionValue(Infinity);
 
   return (
-    <motion.div
-      onMouseMove={(e) => mouseX.set(e.pageX)}
-      onMouseLeave={() => mouseX.set(Infinity)}
-      className="mx-auto flex h-16 items-end gap-4 rounded-2xl border-[1px] border-foreground/5 bg-background/95 px-4 pb-3 backdrop-blur-2xl hover:border-foreground/25"
-    >
-      {mainRoutes.map((route, index) => (
-        <AppIcon
-          mouseX={mouseX}
-          route={route}
-          label={route.label}
-          icon={route.icon}
-          key={index}
-        />
-      ))}
-    </motion.div>
+    <AnimatePresence>
+      <motion.div
+        initial={{ y: initialY, opacity: 1 }} // Initial Y position and opacity
+        animate={{ y: 0, opacity: 1 }} // Animate to final Y position and opacity
+        exit={{ y: initialY, opacity: 1 }} // Exit animation
+        transition={{ delay: animationDelay }} // Delay the animation
+        onMouseMove={(e) => mouseX.set(e.pageX)}
+        onMouseLeave={() => mouseX.set(Infinity)}
+        className="mx-auto flex h-16 items-end gap-4 rounded-2xl border-[1px] border-white/5 bg-black/95 px-4 pb-3 drop-shadow-xl backdrop-blur-sm hover:border-white/25"
+      >
+        {mainRoutes.map((route, index) => (
+          <AppIcon
+            mouseX={mouseX}
+            route={route}
+            label={route.label}
+            icon={route.icon}
+            key={index}
+          />
+        ))}
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
-function AppIcon({
-  mouseX,
-  icon,
-  label,
-  route
-}: {
-  mouseX: MotionValue;
-  icon: any;
-  label: string;
-  route: any;
-}) {
+function AppIcon({ mouseX, route }: { mouseX: MotionValue; route: any }) {
   let ref = useRef<HTMLDivElement>(null);
 
   let distance = useTransform(mouseX, (val) => {
@@ -94,7 +94,7 @@ function AppIcon({
   let widthSync = useTransform(distance, [-150, 0, 150], [40, 100, 40]);
   let width = useSpring(widthSync, { mass: 0.1, stiffness: 150, damping: 12 });
 
-  const IconComponent = icon;
+  const IconComponent = route.icon;
 
   return (
     <TooltipProvider delayDuration={0}>
@@ -104,14 +104,14 @@ function AppIcon({
             <motion.div
               ref={ref}
               style={{ width }}
-              className="flex aspect-square w-10 items-center justify-center rounded-full border-[1px] border-foreground/5 bg-foreground/5 backdrop-blur-2xl transition-colors duration-300 hover:border-foreground/100 hover:bg-background/95" // Apply flex properties for centering
+              className="flex aspect-square w-10 items-center justify-center rounded-full border-[1px] border-white/5 bg-white/5 text-white backdrop-blur-sm transition-colors duration-300 hover:border-white/100 hover:bg-black/95" // Apply flex properties for centering
             >
               <IconComponent style={{ width: '50%', height: '50%' }} />
             </motion.div>
           </Link>
         </TooltipTrigger>
         <TooltipContent>
-          <p>{label}</p>
+          <p>{route.label}</p>
         </TooltipContent>
       </Tooltip>
     </TooltipProvider>
