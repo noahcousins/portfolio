92378dc4365ffe156de02fe8626dc3246626d5d0
Noah Cuz <sales@noahcuz.com>
2024-01-04 17:28:49 -0500

    added useEffect to 404 redirect

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,12 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 export default function Custom404() {
   const router = useRouter();
-  router.push('/');
+  useEffect(() => {
+    router.push('/');
+  }, []);
   return (
     <div className="flex h-screen flex-col space-y-8 text-center">
       <p className="text-center opacity-0">Not found</p>
